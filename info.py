1fd8e7d8799ff480fa3dd1103c81755311b1f2a5
NobitaTheHacker <128245340+NobitaTheHacker@users.noreply.github.com>
2023-05-05 23:30:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,11 +60,11 @@ LAZY_MODE = bool(environ.get("LAZY_MODE"))
 #Add user id of the user in this field those who you want to be Authentic user for file renaming features
 lazy_renamers = [int(lazrenamers) if id_pattern.search(lazrenamers) else lazrenamers for lazrenamers in environ.get('LAZY_RENAMERS', '').split()]
 LAZY_RENAMERS = (lazy_renamers + ADMINS) if lazy_renamers else []
-REQ_CHANNEL = int(environ.get('REQ_CHANNEL'))
+REQ_CHANNEL = int(environ.get('REQ_CHANNEL', '-1001671185048'))
 
 #ai
 AI = is_enabled((environ.get("AI","True")), False)
-OPENAI_API = environ.get("OPENAI_API","")
+OPENAI_API = environ.get("OPENAI_API","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")
 LAZY_AI_LOGS = int(environ.get("LAZY_AI_LOGS","-1001954000385")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of LazyPrincess ]
 # Requested Content template variables ---
 ADMIN_USRNM = environ.get('ADMIN_USRNM','Sad_Nobita') # WITHOUT @
